f1700a413d86f3ff401175b7c38a804ee7f02028
Edson Junior <edson.ascendinojr@gmail.com>
2023-07-27 22:22:05 -0300

    First Commit

--- src/main/java/com/ecore/roles/client/model/Team.java
+++ src/main/java/com/ecore/roles/client/model/Team.java
@@ -1,35 +1,19 @@
 package com.ecore.roles.client.model;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-import javax.persistence.Id;
 import java.util.List;
 import java.util.UUID;
 
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Getter
-@Setter
-@Builder
-@EqualsAndHashCode
 public class Team {
 
-    @Id
-    @JsonProperty
     private UUID id;
-
-    @JsonProperty
     private String name;
-
-    @JsonProperty
     private UUID teamLeadId;
-
-    @JsonProperty
     private List<UUID> teamMemberIds;
 }
